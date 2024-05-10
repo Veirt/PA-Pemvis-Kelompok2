b9286b9a2cebf9387272db397b01562c16e2c9fb
Veirt <veirt@duck.com>
2024-05-10 23:28:36 +0800

    feat: add register validation

--- PA-Kelompok2/Login.vb
+++ PA-Kelompok2/Login.vb
@@ -11,6 +11,41 @@ Public Class Login
         txtPassword.Text = Nothing
     End Sub
 
+    ' Kalo return nya true, berarti exist
+    Public Function IsUsernameExist(username As String)
+
+        CMD = New MySqlCommand($"SELECT COUNT(*) AS count FROM users WHERE username = '{username}'", CONN)
+        RD = CMD.ExecuteReader
+        RD.Read()
+        Dim count = RD("Count")
+        RD.Close()
+
+        Return count = 1
+    End Function
+
+    Public Function ValidateRegister()
+        Dim username = txtUsername.Text
+        Dim password = txtPassword.Text
+
+        ' Validasi username
+        If username.Length < 4 Or username.Length > 20 Then
+            ErrorMsg("Username cannot be less than 4 or more than 20 characters!")
+            Return False
+        End If
+        If IsUsernameExist(username) Then
+            ErrorMsg("Username already exists!")
+            Return False
+        End If
+
+        ' Validasi password
+        If password.Length < 4 Then
+            ErrorMsg("Password cannot be less than 4 characters!")
+            Return False
+        End If
+
+        Return True
+    End Function
+
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
         Dim username = txtUsername.Text
         Dim password = txtPassword.Text
@@ -38,6 +73,10 @@ Public Class Login
     Private Sub btnRegistrasi_Click(sender As Object, e As EventArgs) Handles btnRegistrasi.Click
         Dim username = txtUsername.Text
         Dim password = txtPassword.Text
+
+        If Not ValidateRegister() Then
+            Return
+        End If
 
         Dim query = $"INSERT INTO users (username, password, role) VALUES ('{username}', SHA('{password}'), 'user')"
 
