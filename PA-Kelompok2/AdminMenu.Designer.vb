cfa9a3b1aa44267ea33a690235ed963488bc10b8
IkkiCantCode <ikkiyukkigo@gmail.com>
2024-05-10 23:07:12 +0800

    feat: AdminMenu Design

---
+++ PA-Kelompok2/AdminMenu.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AdminMenu
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblWelcome = New System.Windows.Forms.Label()
+        Me.lblAdmin = New System.Windows.Forms.Label()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        Me.btnRead = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblWelcome
+        '
+        Me.lblWelcome.AutoSize = True
+        Me.lblWelcome.BackColor = System.Drawing.Color.Transparent
+        Me.lblWelcome.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWelcome.Location = New System.Drawing.Point(12, 9)
+        Me.lblWelcome.Name = "lblWelcome"
+        Me.lblWelcome.Size = New System.Drawing.Size(140, 32)
+        Me.lblWelcome.TabIndex = 0
+        Me.lblWelcome.Text = "Welcome,"
+        '
+        'lblAdmin
+        '
+        Me.lblAdmin.AutoSize = True
+        Me.lblAdmin.BackColor = System.Drawing.Color.Transparent
+        Me.lblAdmin.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAdmin.Location = New System.Drawing.Point(143, 9)
+        Me.lblAdmin.Name = "lblAdmin"
+        Me.lblAdmin.Size = New System.Drawing.Size(95, 32)
+        Me.lblAdmin.TabIndex = 1
+        Me.lblAdmin.Text = "Admin"
+        '
+        'btnCreate
+        '
+        Me.btnCreate.BackColor = System.Drawing.Color.FromArgb(CType(CType(83, Byte), Integer), CType(CType(78, Byte), Integer), CType(CType(135, Byte), Integer))
+        Me.btnCreate.ForeColor = System.Drawing.Color.White
+        Me.btnCreate.Location = New System.Drawing.Point(306, 131)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(169, 58)
+        Me.btnCreate.TabIndex = 2
+        Me.btnCreate.Text = "Add Anime"
+        Me.btnCreate.UseVisualStyleBackColor = False
+        '
+        'btnRead
+        '
+        Me.btnRead.BackColor = System.Drawing.Color.FromArgb(CType(CType(83, Byte), Integer), CType(CType(78, Byte), Integer), CType(CType(135, Byte), Integer))
+        Me.btnRead.ForeColor = System.Drawing.Color.White
+        Me.btnRead.Location = New System.Drawing.Point(306, 237)
+        Me.btnRead.Name = "btnRead"
+        Me.btnRead.Size = New System.Drawing.Size(169, 58)
+        Me.btnRead.TabIndex = 3
+        Me.btnRead.Text = "View Anime"
+        Me.btnRead.UseVisualStyleBackColor = False
+        '
+        'AdminMenu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(220, Byte), Integer), CType(CType(215, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnRead)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Controls.Add(Me.lblAdmin)
+        Me.Controls.Add(Me.lblWelcome)
+        Me.Name = "AdminMenu"
+        Me.Text = "AdminMenu"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblWelcome As Label
+    Friend WithEvents lblAdmin As Label
+    Friend WithEvents btnCreate As Button
+    Friend WithEvents btnRead As Button
+End Class
