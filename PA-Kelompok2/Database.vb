5a28832e6376e601ddaa0537192d103197e9dd8d
Veirt <veirt@duck.com>
2024-05-10 21:16:22 +0800

    Add project files.

---
+++ PA-Kelompok2/Database.vb
@@ -0,0 +1,23 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module Database
+    Public CONN As MySqlConnection
+    Public CMD As MySqlCommand
+    Public RD As MySqlDataReader
+    Public DA As MySqlDataAdapter
+    Public DS As DataSet
+    Public CONFIG As String
+    Sub ConnectDatabase()
+        Try
+            CONFIG = "server=localhost;userid=root;password=;database=anime77"
+            'Ganti nama database sesuaikan dengan nama database kalian
+            CONN = New MySqlConnection(CONFIG)
+            If CONN.State = ConnectionState.Closed Then
+                CONN.Open()
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+End Module
---
+++ PA-Kelompok2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PA-Kelompok2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PA-Kelompok2")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("82a57b47-e6b6-4341-b862-f854ae398314")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
